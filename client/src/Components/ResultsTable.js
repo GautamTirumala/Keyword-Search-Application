af3d9ad63cf02432634843e97aa7e8cac0b008a6
Gautam <gautamtirumala6@gmail.com>
2023-09-14 22:01:58 +0530

    Excel data Highlight done, added Testing files

--- client/src/Components/ResultsTable.js
+++ client/src/Components/ResultsTable.js
@@ -5,11 +5,15 @@ function ResultsTable({ results, query }) {
 
   // Function to highlight words in a sentence based on the search query
   const highlightWords = (sentence) => {
+    if (!sentence) return ''; // Add this line to handle undefined or null sentence
+    if (typeof sentence !== 'string') {
+      return sentence; // Return as is if not a string
+    }
     // Use a regular expression to match the query word(s) globally and case-insensitively
     const regex = new RegExp(`\\b(${query.replace(/\s+/g, "|")})\\b`, "gi");
-
+  
     // Split the sentence into an array of words and highlight the matching ones
-    const words = sentence.split(" ").map((word) => {
+    const words = sentence.split(' ').map((word) => {
       if (regex.test(word)) {
         // Apply inline styles for highlighting using HTML span elements
         return `<span style="background-color: yellow; font-weight: bold;">${word}</span>`;
@@ -35,23 +39,35 @@ function ResultsTable({ results, query }) {
             <React.Fragment key={index}>
               {result.fileType === ".xlsx"
                 ? // Special handling for XLSX files
-                  result.results.map((item, itemIndex) => (
-                    <tr key={itemIndex}>
-                      {itemIndex === 0 ? (
-                        <td
-                          rowSpan={result.results.length}
-                          style={{ color: "black" }}
-                        >
-                          {result.fileName}
-                        </td>
-                      ) : null}
+                result.results.map((item, itemIndex) => (
+                  <tr key={itemIndex}>
+                    {itemIndex === 0 ? (
                       <td
-                        dangerouslySetInnerHTML={{
-                          __html: highlightWords(item.rowData.__EMPTY),
-                        }}
-                      />
-                    </tr>
-                  ))
+                        rowSpan={result.results.length}
+                        style={{ color: "black" }}
+                      >
+                        {result.fileName}
+                      </td>
+                    ) : null}
+                    <td>
+                      <pre>
+                        {Object.keys(item.rowData).map((key) => (
+                          <div key={key}>
+                            {key}:{" "}
+                            <span
+                              dangerouslySetInnerHTML={{
+                                __html: highlightWords(item.rowData[key]),
+                              }}
+                            />
+                          </div>
+                        ))}
+                      </pre>
+                    </td>
+                  </tr>
+                )
+                  
+                  
+                  )
                 : // Default handling for other file types
                   result.results.map((item, itemIndex) => (
                     <tr key={itemIndex}>
